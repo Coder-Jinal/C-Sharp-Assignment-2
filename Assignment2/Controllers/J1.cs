afccb0df532f3bc3413b94b098d5fa5a0e14c121
Jinal Patel <n01714049@humber.ca>
2024-10-21 21:02:39 -0400

    Add project files.

---
+++ Assignment2/Controllers/J1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class J1 : Controller
+    {
+        /// <summary>
+        /// Final score is calculated at end of the game of Deliv-e-droid
+        /// </summary>
+        /// <param name="collisions">The number of collisions against the obstacles</param>
+        /// <param name="deliveries">Recieved, the number of deliveries</param>
+        /// <returns>It will return the final score</returns>
+        /// <example>
+        /// POST: https://localhost:7049/api/J1/Delivedroid
+        /// Content-Type: application/x-www-form-urlencoded
+        /// Request Body: collisions=2&deliveries=5
+        /// --> 730
+        /// POST: https://localhost:7049/api/J1/Delivedroid
+        /// Content-Type: application/x-www-form-urlencoded
+        /// Request Body: collisions=10&deliveries=0
+        /// --> -100
+        /// POST: https://localhost:7049/api/J1/Delivedroid
+        /// Content-Type: application/x-www-form-urlencoded
+        /// Request Body: collisions=3&deliveries=2
+        /// --> 70
+        /// </example>
+        [HttpPost(template: "Delivedroid")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public IActionResult Delivedroid ([FromForm]int collisions, [FromForm]int deliveries)
+
+        {
+            int sum = (deliveries * 50) - (collisions * 10);
+
+            if (collisions < deliveries)
+            {
+                sum = sum+500;
+            }
+            return Ok(sum);
+        }
+
+        /// <summary>
+        /// Barley the dog loves treats. At the end of the day he is either happy or sad depending on the number and size of treats he receives 
+        /// throughout the day. The treats come in three sizes: small, medium, and large. His happiness score can be measured using the following 
+        /// formula: 1*S+2*M+3*L where S is the number of small treats, M is the number of medium treats and L is the number of large treats. 
+        /// If Barley’s happiness score is 10 or greater then he is happy. Otherwise, he is sad.Determine whether Barley is happy or sad at the end of the day.
+        /// </summary>
+        /// <param name="small">number of small treats recieved by barley</param>
+        /// <param name="medium">number of medium treats recieved by barley</param>
+        /// <param name="large">number of large treats recieved by barley</param>
+        /// <returns>It returns "Happy" if barley's happiness is 10 or greater, else it will return "Sad"</returns>
+        /// <example>
+        /// POST: https://localhost:7049/api/J1/Barleytreats
+        /// Content-Type: application/x-www-form-urlencoded
+        /// Request Body: small=3&medium=2&large=1
+        /// --> Happy
+        /// POST: https://localhost:7049/api/J1/Barleytreats
+        /// Content-Type: application/x-www-form-urlencoded
+        /// Request Body: small=3&medium=1&large=0
+        /// --> Sad
+        /// </example>
+
+        [HttpPost(template: "Barleytreats")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public IActionResult Barleytreats([FromForm]int small, [FromForm]int medium, [FromForm]int large)
+        {
+            int happiness = (1*small) + (2*medium) + (3*large);
+            if (happiness >= 10)
+            {
+                return Ok("Happy");
+            }
+            else 
+            {
+                return Ok("Sad");
+            }
+        }
+
+    }
+}
---
+++ Assignment2/Controllers/J3.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class J3 : Controller
+    {
+        /// <summary>
+        /// Link: https://cemc.uwaterloo.ca/sites/default/files/documents/2018/2018CCCJrProblemSet.html
+        /// You decide to go for a very long drive on a very straight road. Along this road are five cities. As you travel, you record the distance between each pair of consecutive cities.
+        /// You would like to calculate a distance table that indicates the distance between any two of the cities you have encountered.
+        /// </summary>
+        /// <param name="dis1">the distance encountered between city 1 and city 2</param>
+        /// <param name="dis2">the distance encountered between city 2 and city 3</param>
+        /// <param name="dis3">the distance encountered between city 3 and city 4</param>
+        /// <param name="dis4">the distance encountered between city 4 and city 5</param>
+        /// <returns>It will return a distance table</returns>
+        /// <example>
+        /// GET: "https://localhost:7049/api/J3/calculateddistance?dis1=3&dis2=10&dis3=12&dis4=5" 
+        /// 0 3 13 25 30
+        /// 3 0 10 22 27
+        /// 13 10 0 12 17
+        /// 25 22 12 0 5
+        /// 30 27 17 5 0
+        /// </example>
+        [HttpGet(template:"calculateddistance")]
+        public IActionResult calculateddistance(int dis1, int dis2, int dis3, int dis4 )
+        {
+            int[] distance = new int[5];
+            distance[0] = 0;
+            distance[1] = dis1;
+            distance[2] = dis1+dis2;
+            distance[3] = dis1+dis2+dis3;
+            distance[4] = dis1+dis2+dis3+dis4;
+
+            string totaldistance = "";
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    int distances = Math.Abs(distance[j] - distance[i]);
+                    totaldistance = totaldistance + distances + " ";
+                }
+                totaldistance = totaldistance+"\n";
+            }
+            return Ok(totaldistance);
+        }
+    }
+}
